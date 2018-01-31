eb7bbd643025b17510ca9b9d30b8034e6a27e1eb
Christopher Maier <mail@christophermaier.com.au>
2018-01-31 16:18:00 +1000

    Cleaned up code and added comments

--- Online.rb
+++ Online.rb
@@ -1,47 +1,45 @@
+# Marketing/Propery Development Aggregator App
+# --------------------------------------------
+
+# Gems required
 require 'Nokogiri'
 require 'open-uri'
 require 'net_http_ssl_fix'
 
-output = File.new('titles.txt', 'w+')
+# Create the output file
+output = File.new('output.txt', 'w+')
 
-urls = [
+# Array of all URLs
+links = [
   'http://ee24.com/',
   'http://www.core-me.com/',
   'http://off-planproperties.ae/',
   'https://www.theurbandeveloper.com/',
   'https://www.reidin.com/en-AE',
-  'https://www.masterbuilders.com.au/',
+  # 'https://www.masterbuilders.com.au/',
   'https://propertyeu.info/',
   'https://europroperty.com/',
   'https://pie-mag.com/',
   'https://www.asteco.com/'
 ]
 
+# Array of crawled titles
 titles = []
 
-doc1 = Nokogiri::HTML(open('http://ee24.com/'))
-doc2 = Nokogiri::HTML(open('http://www.core-me.com/'))
-doc3 = Nokogiri::HTML(open('http://off-planproperties.ae/'))
-doc4 = Nokogiri::HTML(open('https://www.theurbandeveloper.com/'))
-doc5 = Nokogiri::HTML(open('https://www.reidin.com/en-AE'))
-# error with masterbuilders
-# doc6 = Nokogiri::HTML(open('https://www.masterbuilders.com.au/'))
-doc7 = Nokogiri::HTML(open('https://propertyeu.info/'))
-doc8 = Nokogiri::HTML(open('https://europroperty.com/'))
-doc9 = Nokogiri::HTML(open('https://pie-mag.com/'))
-doc10 = Nokogiri::HTML(open('https://www.asteco.com/'))
+# Array of crawled image URLs
+images = []
 
-titles << doc1.title
-titles << doc2.title
-titles << doc3.title
-titles << doc4.title
-titles << doc5.title
-# titles << doc6.title
-titles << doc7.title
-titles << doc8.title
-titles << doc9.title
-titles << doc10.title
+# Web crawler logic
+links.each do |url|
+  @doc = Nokogiri::HTML(open(url))
+  titles << @doc.xpath('//head//title').first
+  if @doc.xpath('//img')
+    images << @doc.xpath('//img').first
+  end
+end
 
-output.write(titles)
+# Output all crawled data to text file
+output.write(titles + images)
 
-puts titles
+# Prints contents of titles and images arrays to the screen
+puts titles + images
