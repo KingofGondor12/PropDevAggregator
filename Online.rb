07cb8b104e7f8a665a1468e365f207937751d10f
Chris Hayward <30640240+KingofGondor12@users.noreply.github.com>
2018-01-31 16:36:28 +1000

    Added remaining links + commented out error laden link

--- Online.rb
+++ Online.rb
@@ -7,15 +7,21 @@ output = File.new('titles.txt', 'w+')
 titles = []
 images = []
 links = [
-  'http://ee24.com',
-  'http://www.core-me.com',
-  'http://off-planproperties.ae',
-  'https://www.theurbandeveloper.com',
+  'http://ee24.com/',
+  'http://www.core-me.com/',
+  'http://off-planproperties.ae/',
+  'https://www.theurbandeveloper.com/',
+  'https://www.reidin.com/en-AE',
+  # 'https://www.masterbuilders.com.au/', creates incorrect header check error
+  'https://propertyeu.info/',
+  'https://europroperty.com/',
+  'https://pie-mag.com/',
+  'https://www.asteco.com/'
 ]
 
 links.each do |url|
   @doc = Nokogiri::HTML(open(url))
-  titles << @doc.xpath('//head//title')
+  titles << @doc.xpath('//head//title').first
   if @doc.xpath('//img')
     images << @doc.xpath('//img').first
   end
