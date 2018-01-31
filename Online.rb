df28b9d7dc5a365254acbd674e8282d841c8a460
Christopher Maier <mail@christophermaier.com.au>
2018-01-31 15:11:46 +1000

    Found gem which fixes HTTPS errors 'net_http_ssl_fix'. Now working correctly with both HTTP and HTTPS sources

--- Online.rb
+++ Online.rb
@@ -1,5 +1,6 @@
 require 'Nokogiri'
 require 'open-uri'
+require 'net_http_ssl_fix'
 
 output = File.new('titles.txt', 'w+')
 
@@ -7,9 +8,13 @@ titles = []
 
 doc1 = Nokogiri::HTML(open('http://ee24.com/'))
 doc2 = Nokogiri::HTML(open('http://www.core-me.com/'))
+doc3 = Nokogiri::HTML(open('http://off-planproperties.ae/'))
+doc4 = Nokogiri::HTML(open('https://www.theurbandeveloper.com/'))
 
 titles << doc1.title
 titles << doc2.title
+titles << doc3.title
+titles << doc4.title
 
 output.write(titles)
 
