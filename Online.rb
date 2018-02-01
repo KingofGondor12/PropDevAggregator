83389292f83ef328918627b7d35d7b276be58c80
Christopher Maier <mail@christophermaier.com.au>
2018-02-01 10:39:49 +1000

    Merged all code with Chris H's code.

--- Online.rb
+++ Online.rb
@@ -11,16 +11,16 @@ output = File.new('output.txt', 'w+')
 
 # Array of all URLs
 links = [
-  'http://ee24.com/',
-  'http://www.core-me.com/',
-  'http://off-planproperties.ae/',
-  'https://www.theurbandeveloper.com/',
+  'http://ee24.com',
+  'http://www.core-me.com',
+  'http://off-planproperties.ae',
+  'https://www.theurbandeveloper.com',
   'https://www.reidin.com/en-AE',
-  # 'https://www.masterbuilders.com.au/',
-  'https://propertyeu.info/',
-  'https://europroperty.com/',
-  'https://pie-mag.com/',
-  'https://www.asteco.com/',
+  # 'https://www.masterbuilders.com.au/', creates incorrect header check error
+  'https://propertyeu.info',
+  'https://europroperty.com',
+  'https://pie-mag.com',
+  'https://www.asteco.com'
 ]
 
 # Array of crawled results
@@ -29,11 +29,18 @@ results = []
 # Web crawler logic
 links.each do |url|
   @doc = Nokogiri::HTML(open(url))
-  @title = @doc.xpath('//head//title/text()').first.text
+  @title = @doc.xpath('//head//title').first.content
+  @title = @title.squeeze(" ").gsub(/[^a-zA-Z0-9. ]/, '')
+  if @title[0] == " "
+    @title = @title.strip
+  end
   if @doc.xpath('//img')
     @image = @doc.xpath('//img//@src').first.content
+    if @image.slice(0, 4) != "http"
+      @image = "#{url}#{@image}"
+    end
   end
-  results << {name: @title, image: @image}
+  results << {name: @title, logo: @image}
 end
 
 # Output all crawled data to text file
