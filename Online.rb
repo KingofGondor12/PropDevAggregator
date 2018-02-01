995ae564fa3b3dbb378c562b8d98e4249f701a6d
Chris Hayward <30640240+KingofGondor12@users.noreply.github.com>
2018-02-01 16:52:48 +1000

    Changed image type comparison to not utilise ruby-filemagic

--- Online.rb
+++ Online.rb
@@ -5,7 +5,7 @@
 require 'Nokogiri'
 require 'open-uri'
 require 'net_http_ssl_fix'
-require 'filemagic'
+require 'image_size'
 
 # Create the output file
 output = File.new('output.txt', 'w+')
@@ -15,7 +15,7 @@ links = [
   'http://ee24.com',
   'http://www.core-me.com',
   'http://off-planproperties.ae',
-  'https://www.theurbandeveloper.com',
+  'https://theurbandeveloper.com',
   'https://www.reidin.com/en-AE',
   # 'https://www.masterbuilders.com.au/', creates incorrect header check error
   'https://propertyeu.info',
@@ -37,13 +37,16 @@ links.each do |url|
     @title = @title.strip
   end
   @images = @doc.xpath('//img//@src')
+  @images = @images
+  puts @doc.xpath('substring-before(substring-after(//div/@style, "background: url('"), "')")')
   @images.each do |image|
     if image.content.slice(0, 4) != "http"
       image = "#{url}#{image.content}"
-      if FileMagic.open(:mime) { |fm|
-        fm.file(image, true)
-      } == "image/jpeg"
-        @imagesArray << image
+      if image.slice(-3, 3) == 'jpg'
+        # @size_test = ImageSize.path(image)
+        # if @size_test.width >= 1024 && @size_test.height >= 768
+          @imagesArray << image
+        # end
       end
     end
   end
