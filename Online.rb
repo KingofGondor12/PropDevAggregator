825d2668e553f0af7e60e87d8b7516e8c2fa5760
Chris Hayward <30640240+KingofGondor12@users.noreply.github.com>
2018-02-01 10:09:38 +1000

    Added if statement for image links & Added RegEx to clip title whitespace

--- Online.rb
+++ Online.rb
@@ -7,25 +7,28 @@ output = File.new('titles.txt', 'w+')
 results = []
 
 links = [
-  'http://ee24.com/',
-  'http://www.core-me.com/',
-  'http://off-planproperties.ae/',
-  'https://www.theurbandeveloper.com/',
+  'http://ee24.com',
+  'http://www.core-me.com',
+  'http://off-planproperties.ae',
+  'https://www.theurbandeveloper.com',
   'https://www.reidin.com/en-AE',
   # 'https://www.masterbuilders.com.au/', creates incorrect header check error
-  'https://propertyeu.info/',
-  'https://europroperty.com/',
-  'https://pie-mag.com/',
-  'https://www.asteco.com/'
+  'https://propertyeu.info',
+  'https://europroperty.com',
+  'https://pie-mag.com',
+  'https://www.asteco.com'
 ]
 
 links.each do |url|
   @doc = Nokogiri::HTML(open(url))
-  @title = @doc.xpath('//head//title').first.text
+  @title = @doc.xpath('//head//title').first.content
   if @doc.xpath('//img')
-    @image = @doc.xpath('//img').first
+    @image = @doc.xpath('//img//@src').first.content
+    if @image.slice(0, 4) != "http"
+      @image = "#{url}#{@image}"
+    end
   end
-  results << {name: @title, logo: @image}
+  results << {name: @title.squeeze(" ").gsub(/[^a-zA-Z0-9. ]/, ''), logo: @image}
 end
 
 output.write(results)
