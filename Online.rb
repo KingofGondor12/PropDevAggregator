25a2b488ebc0245a341a530a576b336129028c92
Christopher Maier <mail@christophermaier.com.au>
2018-02-02 11:41:21 +1000

    Urban Developer web crawling images to one large string.

--- Online.rb
+++ Online.rb
@@ -10,38 +10,27 @@ require 'net_http_ssl_fix'
 output = File.new('output.txt', 'w+')
 
 # Array of all URLs
-links = [
-  'http://ee24.com',
-  'http://www.core-me.com',
-  'http://off-planproperties.ae',
-  'https://www.theurbandeveloper.com',
-  'https://www.reidin.com/en-AE',
-  # 'https://www.masterbuilders.com.au/', creates incorrect header check error
-  'https://propertyeu.info',
-  'https://europroperty.com',
-  'https://pie-mag.com',
-  'https://www.asteco.com'
-]
+# links = [
+#   'https://www.theurbandeveloper.com/category/Property%20Development',
 
 # Array of crawled results
 results = []
 
 # Web crawler logic
-links.each do |url|
-  @doc = Nokogiri::HTML(open(url))
-  @title = @doc.xpath('//head//title').first.content
-  @title = @title.squeeze(" ").gsub(/[^a-zA-Z0-9. ]/, '')
-  if @title[0] == " "
-    @title = @title.strip
-  end
-  if @doc.xpath('//img')
-    @image = @doc.xpath('//img//@src').first.content
-    if @image.slice(0, 4) != "http"
-      @image = "#{url}#{@image}"
-    end
-  end
-  results << {name: @title, logo: @image}
-end
+@doc = Nokogiri::HTML(open('https://www.theurbandeveloper.com/category/Property%20Development'))
+@image = @doc.xpath('//picture/img//@src').text
+#   @title = @title.squeeze(" ").gsub(/[^a-zA-Z0-9. ]/, '')
+#   if @title[0] == " "
+#     @title = @title.strip
+#   end
+#   if @doc.xpath('//img')
+#     @image = @doc.xpath('//img//@src').first.content
+#     if @image.slice(0, 4) != "http"
+#       @image = "#{url}#{@image}"
+#     end
+#   end
+  results << {name: @title, image: @image}
+# end
 
 # Output all crawled data to text file
 output.write(results)
