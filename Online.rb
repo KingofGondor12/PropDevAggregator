53c94bec7c8fb7d215602301003fbc0dd3b2c18d
Chris Hayward <30640240+KingofGondor12@users.noreply.github.com>
2018-02-06 12:57:45 +1000

    Finally fixed bugs, cake wanted

--- Online.rb
+++ Online.rb
@@ -48,34 +48,63 @@ links.each do |url|
       temp = div.index("url(").to_i
       temp2 = "url(".size.to_i
       temp3 = (temp + temp2)
-      for i in temp3..div.length-1
-        if div[i] == ")"
-          break
+
+      if div[temp3] == "'"
+        for i in (temp3 + 1)..div.length-1
+          if div[i] == "'"
+            break
+          end
+          array << div[i]
         end
-        array << div[i]
-      end
-      array = array.join('').to_s
-      if array.start_with?("http")
-        array.delete("'")
+        array = array.join('').to_s
+        if !array.start_with?("http")
+          image = array.insert(0, url)
+        end
+        image = array
+      elsif div[temp3] == '"'
+        for i in (temp3 + 1)..div.length-1
+          if div[i] == '"'
+            break
+          end
+          array << div[i]
+        end
+        array = array.join('').to_s
+        if !array.start_with?("http")
+          image = array.insert(0, url)
+        end
+        image = array
       else
-        array.delete("'")
-        image = array.insert(0, url)
+        for i in temp3..div.length-1
+          if div[i] == ")"
+            break
+          end
+          array << div[i]
+        end
+        array = array.join('').to_s
+        if !array.start_with?("http")
+          image = array.insert(0, url)
+        end
+        image = array
       end
-      @imagesArray << image
+      if image.slice(-3, 3) == "jpg"
+        @imagesArray << image
+      elsif image.slice(-4, 4) == "jpeg"
+        @imagesArray << image
+      end
     end
   end
 
-  # @images.each do |image|
-  #   if image.content.slice(0, 4) != "http"
-  #     image = "#{url}#{image.content}"
-  #     if image.slice(-3, 3) == 'jpg'
-  #       # @size_test = ImageSize.path(image)
-  #       # if @size_test.width >= 1024 && @size_test.height >= 768
-  #         @imagesArray << image
-  #       # end
-  #     end
-  #   end
-  # end
+  @images.each do |image|
+    if image.content.slice(0, 4) != "http"
+      image = "#{url}#{image.content}"
+      if image.slice(-3, 3) == 'jpg'
+        # @size_test = ImageSize.path(image)
+        # if @size_test.width >= 1024 && @size_test.height >= 768
+          @imagesArray << image
+        # end
+      end
+    end
+  end
   results << {name: @title, images: @imagesArray}
 end
 
