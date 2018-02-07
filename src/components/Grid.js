e09d0a8b54d4622fa63b422fa8e2f329e342705a
Chris Hayward <30640240+KingofGondor12@users.noreply.github.com>
2018-02-07 14:49:54 +1000

    Added map function to iterate through siteData

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import {} from 'semantic-ui-react';
 
 class Grid extends Component {
 
@@ -11,75 +10,20 @@ class Grid extends Component {
     return (
       <div className="column">
         <div className="ui link cards">
-          <div className="card">
-            <div className="image">
-              <img src="/images/avatar2/large/matthew.png" />
-            </div>
-            <div className="content">
-              <div className="header">Matt Giampietro</div>
-              <div className="meta">
-                <a>Friends</a>
+        {
+          siteData.map((hash) => (
+            <div className="card">
+              <div className="image">
+                <img src={hash.image} />
               </div>
-              <div className="description">
-                Matthew is an interior designer living in New York.
-              </div>
-            </div>
-            <div className="extra content">
-              <span className="right floated">
-                Joined in 2013
-              </span>
-              <span>
-                <i className="user icon"></i>
-                75 Friends
-              </span>
-            </div>
-          </div>
-          <div className="card">
-            <div className="image">
-              <img src="/images/avatar2/large/molly.png" />
-            </div>
-            <div className="content">
-              <div className="header">Molly</div>
-              <div className="meta">
-                <span className="date">Coworker</span>
-              </div>
-              <div className="description">
-                Molly is a personal assistant living in Paris.
+              <div className="content">
+                <a href={hash.url}>
+                  {hash.title}
+                </a>
               </div>
             </div>
-            <div className="extra content">
-              <span className="right floated">
-                Joined in 2011
-              </span>
-              <span>
-                <i className="user icon"></i>
-                35 Friends
-              </span>
-            </div>
-          </div>
-          <div className="card">
-            <div className="image">
-              <img src="/images/avatar2/large/elyse.png" />
-            </div>
-            <div className="content">
-              <div className="header">Elyse</div>
-              <div className="meta">
-                <a>Coworker</a>
-              </div>
-              <div className="description">
-                Elyse is a copywriter working in New York.
-              </div>
-            </div>
-            <div className="extra content">
-              <span className="right floated">
-                Joined in 2014
-              </span>
-              <span>
-                <i className="user icon"></i>
-                151 Friends
-              </span>
-            </div>
-          </div>
+          ))
+        }
         </div>
       </div>
     );
