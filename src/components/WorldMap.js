99ab7069efe8b9ec0a9845d179f6c8f5be3e3df8
Chris Hayward <30640240+KingofGondor12@users.noreply.github.com>
2018-02-13 09:39:09 +1000

    Edited Lat-Long to correct marker position

--- src/components/WorldMap.js
+++ src/components/WorldMap.js
@@ -28,20 +28,11 @@ class WorldMap extends Component {
     super(props);
 
     this.state = {
-      dataProvider: generateData(),
-      timer: null
+      dataProvider: {
+        "map": "worldLow",
+        "getAreasFromMap": true
+      },
     };
-  }
-
-  componentDidMount() {
-    this.setState({
-      // Update the chart dataProvider every 3 seconds
-      timer: setInterval(() => {
-        this.setState({
-          dataProvider: generateData()
-        });
-      }, 3000)
-    });
   }
 
   componentWillUnmount() {
@@ -50,76 +41,10 @@ class WorldMap extends Component {
 
   render() {
     const config = {
-      "type": "serial",
+      "type": "map",
       "theme": "light",
-      "marginRight": 40,
-      "marginLeft": 40,
-      "autoMarginOffset": 20,
-      "mouseWheelZoomEnabled": true,
-      "valueAxes": [{
-        "id": "v1",
-        "axisAlpha": 0,
-        "position": "left",
-        "ignoreAxisWidth": true
-      }],
-      "balloon": {
-        "borderThickness": 1,
-        "shadowAlpha": 0
-      },
-      "graphs": [{
-        "id": "g1",
-        "balloon":{
-          "drop": true,
-          "adjustBorderColor": false,
-          "color":"#ffffff"
-        },
-        "bullet": "round",
-        "bulletBorderAlpha": 1,
-        "bulletColor": "#FFFFFF",
-        "bulletSize": 5,
-        "hideBulletsCount": 50,
-        "lineThickness": 2,
-        "title": "red line",
-        "useLineColorForBulletBorder": true,
-        "valueField": "value",
-        "balloonText": "<span style='font-size:18px;'>[[value]]</span>"
-      }],
-      "chartScrollbar": {
-        "graph": "g1",
-        "oppositeAxis": false,
-        "offset":30,
-        "scrollbarHeight": 80,
-        "backgroundAlpha": 0,
-        "selectedBackgroundAlpha": 0.1,
-        "selectedBackgroundColor": "#888888",
-        "graphFillAlpha": 0,
-        "graphLineAlpha": 0.5,
-        "selectedGraphFillAlpha": 0,
-        "selectedGraphLineAlpha": 1,
-        "autoGridCount": true,
-        "color":"#AAAAAA"
-      },
-      "chartCursor": {
-        "pan": true,
-        "valueLineEnabled": true,
-        "valueLineBalloonEnabled": true,
-        "cursorAlpha":1,
-        "cursorColor":"#258cbb",
-        "limitToGraph":"g1",
-        "valueLineAlpha":0.2,
-        "valueZoomable": true
-      },
-      "valueScrollbar":{
-        "oppositeAxis": false,
-        "offset":50,
-        "scrollbarHeight":10
-      },
-      "categoryField": "date",
-      "categoryAxis": {
-        "parseDates": true,
-        "dashLength": 1,
-        "minorGridEnabled": true
-      },
+      "autoZoom": true,
+      "selectedColor": "#CC0000",
       "dataProvider": this.state.dataProvider
     };
 
