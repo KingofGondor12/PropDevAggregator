ee774ddf865365ad3e8e0615dc2f2c922cef7dd2
Christopher Maier <mail@christophermaier.com.au>
2018-02-14 13:26:33 +1000

    Map styling and functionality. Cleaned up code and archived old code.

--- src/components/WorldMap.js
+++ src/components/WorldMap.js
@@ -16,21 +16,24 @@ class WorldMap extends Component {
               "id": "bg",
                 "stop": [{
                   "offset": "0%",
-                  "stop-color": "#2D7FBB"
+                  "stop-color": "#4ea5e6"
                 },{
                   "offset": "100%",
-                  "stop-color": "#dbedff"
+                  "stop-color": "#baddff"
                 }]
             },
             {
             "id": "sample2",
               "stop": [{
                 "offset": "0%",
-                "stop-color": "#0093ff"
+                "stop-color": "#ff4b7e"
+              },{
+                "offset": "50%",
+                "stop-color": "#ff5a4b"
               },{
                 "offset": "100%",
-                "stop-color": "#00950b"
-              }]
+                "stop-color": "#ff9318"
+              }],
             },
           ],
           },
@@ -41,44 +44,68 @@ class WorldMap extends Component {
 					"dataProvider": {
 						"map": "worldLow",
 						"getAreasFromMap": true,
-						"images": [
-							{
-								"top": 40,
-								"left": 60,
-								"width": 80,
-								"height": 40,
-							},
-              {
-								"selectable": true,
-                title: "<a href='#search'><div class='sydDesc'><h1>Australia</h1><h3>Click to view all articles and resources related to this location.</h3></div></a>",
-								"longitude": 151.209444,
-								"latitude": -33.865,
-								"svgPath": "M3.5,13.277C3.5,6.22,9.22,0.5,16.276,0.5C23.333,0.5,29.053,6.22,29.053,13.277C29.053,14.54,28.867,15.759,28.526,16.914C26.707,24.271,16.219,32.5,16.219,32.5C16.219,32.5,4.37,23.209,3.673,15.542C3.673,15.542,3.704,15.536,3.704,15.536C3.572,14.804,3.5,14.049,3.5,13.277C3.5,13.277,3.5,13.277,3.5,13.277M16.102,16.123C18.989,16.123,21.329,13.782,21.329,10.895C21.329,8.008,18.989,5.668,16.102,5.668C13.216,5.668,10.876,8.008,10.876,10.895C10.876,13.782,13.216,16.123,16.102,16.123C16.102,16.123,16.102,16.123,16.102,16.123",
-								"color": "rgba(255, 102, 145, 1)",
-								"scale": 1,
-                "myUrl": "#search"
-							},
-							{
-								"selectable": true,
-                title: "<a href='#search'><div class='beijingDesc'><h1>China</h1><h3>Click to view all articles and resources related to this location.</h3></div></a>",
-								"longitude": 116.383333,
-								"latitude": 39.916667,
-								"svgPath": "M3.5,13.277C3.5,6.22,9.22,0.5,16.276,0.5C23.333,0.5,29.053,6.22,29.053,13.277C29.053,14.54,28.867,15.759,28.526,16.914C26.707,24.271,16.219,32.5,16.219,32.5C16.219,32.5,4.37,23.209,3.673,15.542C3.673,15.542,3.704,15.536,3.704,15.536C3.572,14.804,3.5,14.049,3.5,13.277C3.5,13.277,3.5,13.277,3.5,13.277M16.102,16.123C18.989,16.123,21.329,13.782,21.329,10.895C21.329,8.008,18.989,5.668,16.102,5.668C13.216,5.668,10.876,8.008,10.876,10.895C10.876,13.782,13.216,16.123,16.102,16.123C16.102,16.123,16.102,16.123,16.102,16.123",
-								"color": "rgba(255, 102, 145, 1)",
-								"scale": 1,
-                "myUrl": "#search"
-							},
-							{
-								"selectable": true,
-                title: "<a href='#search'><div class='dubaiDesc'><h1>Dubai & UAE</h1><h3>Click to view all articles and resources related to this location.</h3></div></a>",
-								"longitude": 55.297222,
-								"latitude": 25.263056,
-								"svgPath": "M3.5,13.277C3.5,6.22,9.22,0.5,16.276,0.5C23.333,0.5,29.053,6.22,29.053,13.277C29.053,14.54,28.867,15.759,28.526,16.914C26.707,24.271,16.219,32.5,16.219,32.5C16.219,32.5,4.37,23.209,3.673,15.542C3.673,15.542,3.704,15.536,3.704,15.536C3.572,14.804,3.5,14.049,3.5,13.277C3.5,13.277,3.5,13.277,3.5,13.277M16.102,16.123C18.989,16.123,21.329,13.782,21.329,10.895C21.329,8.008,18.989,5.668,16.102,5.668C13.216,5.668,10.876,8.008,10.876,10.895C10.876,13.782,13.216,16.123,16.102,16.123C16.102,16.123,16.102,16.123,16.102,16.123",
-								"color": "rgba(255, 102, 145, 1)",
-								"scale": 1,
-                "myUrl": "#search"
-							}
-						]
+            "areas":
+              [
+                {
+                  id: "AU",
+                  color: "#F37B86",
+                  url: "#search",
+                  title: "<a href='#search'><div class='sydDesc'><h1>Australia</h1><h3>Click to view all articles and resources related to this location.</h3></div></a>"
+                },{
+                  id: "AE",
+                  color: "#FEE7A5",
+                  url: "#search",
+                  title: "<a href='#search'><div class='dubaiDesc'><h1>Dubai & UAE</h1><h3>Click to view all articles and resources related to this location.</h3></div></a>"
+                },{
+                  id: "CN",
+                  color: "#3FA4E6",
+                  url: "#search",
+                  title: "<a href='#search'><div class='beijingDesc'><h1>China</h1><h3>Click to view all articles and resources related to this location.</h3></div></a>"
+                },{
+                  id: "US",
+                  color: "#27358E",
+                  url: "#search",
+                  title: "<a href='#search'><div class='usaDesc'><h1>United States</h1><h3>Click to view all articles and resources related to this location.</h3></div></a>"
+                }
+              ],
+						// "images": [
+						// 	{
+						// 		"top": 40,
+						// 		"left": 60,
+						// 		"width": 80,
+						// 		"height": 40,
+						// 	},
+            //   {
+						// 		"selectable": true,
+            //     title: "<a href='#search'><div class='sydDesc'><h1>Australia</h1><h3>Click to view all articles and resources related to this location.</h3></div></a>",
+						// 		"longitude": 151.209444,
+						// 		"latitude": -33.865,
+						// 		"svgPath": "M3.5,13.277C3.5,6.22,9.22,0.5,16.276,0.5C23.333,0.5,29.053,6.22,29.053,13.277C29.053,14.54,28.867,15.759,28.526,16.914C26.707,24.271,16.219,32.5,16.219,32.5C16.219,32.5,4.37,23.209,3.673,15.542C3.673,15.542,3.704,15.536,3.704,15.536C3.572,14.804,3.5,14.049,3.5,13.277C3.5,13.277,3.5,13.277,3.5,13.277M16.102,16.123C18.989,16.123,21.329,13.782,21.329,10.895C21.329,8.008,18.989,5.668,16.102,5.668C13.216,5.668,10.876,8.008,10.876,10.895C10.876,13.782,13.216,16.123,16.102,16.123C16.102,16.123,16.102,16.123,16.102,16.123",
+						// 		"color": "#ff6691",
+						// 		"scale": 1,
+            //     "myUrl": "#search"
+						// 	},
+						// 	{
+						// 		"selectable": true,
+            //     title: "<a href='#search'><div class='beijingDesc'><h1>China</h1><h3>Click to view all articles and resources related to this location.</h3></div></a>",
+						// 		"longitude": 116.383333,
+						// 		"latitude": 39.916667,
+						// 		"svgPath": "M3.5,13.277C3.5,6.22,9.22,0.5,16.276,0.5C23.333,0.5,29.053,6.22,29.053,13.277C29.053,14.54,28.867,15.759,28.526,16.914C26.707,24.271,16.219,32.5,16.219,32.5C16.219,32.5,4.37,23.209,3.673,15.542C3.673,15.542,3.704,15.536,3.704,15.536C3.572,14.804,3.5,14.049,3.5,13.277C3.5,13.277,3.5,13.277,3.5,13.277M16.102,16.123C18.989,16.123,21.329,13.782,21.329,10.895C21.329,8.008,18.989,5.668,16.102,5.668C13.216,5.668,10.876,8.008,10.876,10.895C10.876,13.782,13.216,16.123,16.102,16.123C16.102,16.123,16.102,16.123,16.102,16.123",
+						// 		"color": "rgba(255, 102, 145, 1)",
+						// 		"scale": 1,
+            //     "myUrl": "#search"
+						// 	},
+						// 	{
+						// 		"selectable": true,
+            //     title: "<a href='#search'><div class='dubaiDesc'><h1>Dubai & UAE</h1><h3>Click to view all articles and resources related to this location.</h3></div></a>",
+						// 		"longitude": 55.297222,
+						// 		"latitude": 25.263056,
+						// 		"svgPath": "M3.5,13.277C3.5,6.22,9.22,0.5,16.276,0.5C23.333,0.5,29.053,6.22,29.053,13.277C29.053,14.54,28.867,15.759,28.526,16.914C26.707,24.271,16.219,32.5,16.219,32.5C16.219,32.5,4.37,23.209,3.673,15.542C3.673,15.542,3.704,15.536,3.704,15.536C3.572,14.804,3.5,14.049,3.5,13.277C3.5,13.277,3.5,13.277,3.5,13.277M16.102,16.123C18.989,16.123,21.329,13.782,21.329,10.895C21.329,8.008,18.989,5.668,16.102,5.668C13.216,5.668,10.876,8.008,10.876,10.895C10.876,13.782,13.216,16.123,16.102,16.123C16.102,16.123,16.102,16.123,16.102,16.123",
+						// 		"color": "rgba(255, 102, 145, 1)",
+						// 		"scale": 1,
+            //     "myUrl": "#search"
+						// 	}
+						// ]
 					},
           "balloon": {
             "color": "#000",
@@ -93,10 +120,10 @@ class WorldMap extends Component {
 						"color": "#FFF",
 						"outlineColor": 0,
 						"rollOverOutlineColor": 0,
-						"rollOverColor": "#1ccfab",
+						// "rollOverColor": "#1ccfab",
 						"selectedColor": "#1cc8cf",
-						"selectable": true,
-            "autoZoom": true,
+						"selectable": false,
+            "autoZoom": false,
 						"unlistedAreasAlpha": 0,
 						"unlistedAreasOutlineAlpha": 0
 					},
@@ -126,17 +153,17 @@ class WorldMap extends Component {
 						"gridHeight": 100,
 						"gridAlpha": 0.5,
 						"gridBackgroundAlpha": 0,
-						"gridColor": "#FFFFFF",
+						"gridColor": "#FFF",
 						"draggerAlpha": 1,
 						"buttonCornerRadius": 2
 					},
           "smallMap": {},
-          "listeners": [ {
-            "event": "clickMapObject",
-            "method": function( event ) {
-                window.location.href = event.mapObject.myUrl;
-            }
-          }]
+          // "listeners": [ {
+          //   "event": "clickMapObject",
+          //   "method": function( event ) {
+          //       window.location.href = event.mapObject.myUrl;
+          //   }
+          // }]
     }
 
     function clickObject( id ) {
